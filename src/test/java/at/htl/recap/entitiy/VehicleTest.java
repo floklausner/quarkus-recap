70e004ec04956822c6a7138cd26b04f1709a0e5c
floklausner <klausnerflo@gmail.com>
2021-06-01 12:40:55 +0200

    VehicleTest

---
+++ src/test/java/at/htl/recap/entitiy/VehicleTest.java
@@ -0,0 +1,15 @@
+package at.htl.recap.entitiy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @Test
+    void t010_checkAgeMethod() {
+
+        Vehicle vehicle = new Vehicle("VW", "Käfer", 1969);
+
+    }
+}
