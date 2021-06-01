e24ac2b7afeb8f688064b0d4a82bd817f8b25bf0
floklausner <klausnerflo@gmail.com>
2021-06-01 12:04:00 +0200

    initial commit

---
+++ src/main/java/at/htl/recap/VehicleRepository.java
@@ -0,0 +1,12 @@
+package at.htl.recap;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class VehicleRepository implements PanacheRepository {
+
+
+
+}
